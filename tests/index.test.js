6a4a4be6047829dc15f1e58661f05168caaf506e
jeronimobarbieri <jeronimo.barbieri@fatec.sp.gov.br>
2025-08-14 12:01:21 -0300

    atualização blog

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-test('HTML deve conter título e imagem', () => {
+test('HTML deve conter título e contador', () => {
     const htmlPath = path.join(__dirname, '../src/index.html');
     const html = fs.readFileSync(htmlPath, 'utf8');
 
-    expect(html).toContain('<h1>Bem-vindo ao meu projeto!</h1>');
-    expect(html).toContain('<img src="https://cdn.eadplataforma.app/client/dcpi1/upload/crop/product/photo/890bb2a481fb3a9a1e5acad2ea61e44f_introducao-a-freudjpg.jpg-product-photo-detail-mobile.jpg"');
+    expect(html).toContain('<h1>Blog de Psicanálise Clínica</h1>');
+    expect(html).toContain('id="contador"');
 });
